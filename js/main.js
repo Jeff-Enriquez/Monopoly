1d73b1f32e447af9b2b37ff45a4079e5ae984785
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2019-12-12 08:15:34 -0800

    PseudoCode

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
+
+/*
+
+init() {
+  players given 1500 in money
+  players start at GO
+  All properties are unsold
+  Shuffle Chance and Community Chest
+  player 1 rolls first
+}
+render() {
+  playermove()
+  propertyPurchase()
+  renderDisplay()
+  playerLost()
+}
+playermove(){
+  roll dice
+}
+playerLost() {
+  if no money
+}
+
+propertyPurchase() {
+  playerMoney -= propertyCost
+  property.propertyName.owner = playerName;
+}
+
+display() {
+  location of player
+  player money
+  update player history
+  if(winner())
+}
+
+winner() {
+  all players - 1, have 0 money
+}
+*/
+
 /*----- constants -----*/
 
 /*----- app's state (variables) -----*/
